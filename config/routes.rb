926d1262a739fe8d1aefacfdf0d1014067c314a7
thangtx <tranxuanthang2204@gmail.com>
2016-04-27 16:48:36 +0700

    Login/Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  get "sessions/new"
   get "signup"  => "users#new"
+  get    "login"   => "sessions#new"
+  post   "login"   => "sessions#create"
+  delete "logout"  => "sessions#destroy"
   resources :users
 end
