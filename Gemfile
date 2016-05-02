cd967cc9562970e7c25789ae6d6a6abf801d3420
thangtx <tranxuanthang2204@gmail.com>
2016-05-02 09:33:20 +0700

    show_update_user

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem "bootstrap-sass",       "3.2.0.0"
 
 gem "bcrypt",               "3.1.7"
 
+gem "faker",                "1.4.2"
+
 # Use jquery as the JavaScript library
 gem "jquery-rails"
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update]
   def new
     @user = User.new
   end
@@ -13,6 +14,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "views.update.success"
+      redirect_to @user
+    else
+      flash[:danger] = t "views.update.error"
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
   def show
     @user = User.find params[:id]
   end
@@ -21,5 +35,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def find_user
+    @user = User.find params[:id]
   end
 end
