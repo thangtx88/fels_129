5b1df8ce720f9e2c42b068b0da68bb346f7e111b
thangtx <tranxuanthang2204@gmail.com>
2016-05-09 11:04:40 +0700

    admin_show_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,15 +18,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
-  def show_data_user
-    if logged_in?
-      @categories = Category.all
-      @words = Word.send("by_learned", current_user.id, @categories.map(&:id))
-        .paginate page: params[:page]
-      @count = @words.present? ? @words.count : 0
-      @activities = current_user.activities.order(created_at: :DESC)
-        .paginate page: params[:page], per_page: Settings.per_page
-    end
-  end
+  def show_data_user user
+    @categories = Category.all
+    @words = Word.send("by_learned", user.id, @categories.map(&:id))
+      .paginate page: params[:page]
+    @count = @words.present? ? @words.count : 0
+    @activities = user.activities.order(created_at: :DESC)
+      .paginate page: params[:page], per_page: Settings.per_page
+   end
 end
