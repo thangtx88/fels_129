789c465cce62bca2e8e9e1dfed8a40212ca0b828
thangtx <tranxuanthang2204@gmail.com>
2016-05-10 13:12:22 +0700

    remove_code_cu_for_gem_devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,22 +10,12 @@ class User < ActiveRecord::Base
   before_save {self.email = email.downcase}
 
   validates :name, presence: true, length: {maximum: 50}
-  validates :password, length: {minimum: 6}, allow_blank: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
 
-  has_secure_password
   mount_uploader :avatar, AvatarUploader
-
-  class << self
-    def digest string
-      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-        BCrypt::Engine.cost
-      BCrypt::Password.create string, cost: cost
-    end
-  end
 
   def follow other_user
     active_relationships.create followed_id: other_user.id
